7310e4d72391466fb3c147703de699ceed93a1b1
Louis Xu <louisxu2003@gmail.com>
2024-01-19 12:22:59 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+import VisualMemoryPage from "./assets/pages/VisualMemoryPage";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Header from "./assets/components/main/header";
+import Home from "./assets/pages/Home";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="/VisualMemory" element={<VisualMemoryPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
